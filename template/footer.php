0f7e629c508e0f03de58443637f61f14419dd8b8
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-25 11:46:56 +0200

    modifier style

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
 <!-- footer.php -->
-<footer class="mon_footer  text-center text-lg-start mt-auto ">
+<footer class="mon_footer  text-center text-lg-start mt-auto">
   <div class="text-center p-3">
     © 2021 Copyright  
     <a class="text-dark" href="#">Hackers Poulette ™</a>
--- template/header.php
+++ template/header.php
@@ -3,7 +3,7 @@
 
 
 
-<nav class="mon_header navbar navbar-expand-lg navbar-light ">
+<nav class="mon_header navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Hackers Poulette ™</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
